6aed170744ba918b9dd318ec5a25e3509c4038ad
Brian Ho <briankyho@gmail.com>
2017-11-01 02:36:56 -0700

    Small refactor and name saved to shared preferences

--- app/src/main/java/ca/brianho/avaloo/LobbyActivity.kt
+++ app/src/main/java/ca/brianho/avaloo/LobbyActivity.kt
@@ -2,13 +2,22 @@ package ca.brianho.avaloo
 
 import android.app.Activity
 import android.os.Bundle
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.defaultSharedPreferences
+import org.jetbrains.anko.info
 
-class LobbyActivity : Activity() {
+class LobbyActivity : Activity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lobby)
 
-        replaceFragment(R.id.fragment_container, NameFragment())
+        val name = defaultSharedPreferences.getString(getString(R.string.key_name), null)
+
+        if (name == null) {
+            replaceFragment(R.id.fragment_container, NameFragment())
+        } else {
+            info("Saved name is " + name)
+        }
     }
 }
--- app/src/main/java/ca/brianho/avaloo/NameFragment.kt
+++ app/src/main/java/ca/brianho/avaloo/NameFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_name.*
 import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.defaultSharedPreferences
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 
@@ -27,14 +28,14 @@ class NameFragment : Fragment(), AnkoLogger {
     }
 
     private fun onNextClicked() {
-        if (nameField.text.isNotEmpty()) {
-            val name = nameField.text
-
+        if (nameField.text.isNullOrBlank()) {
+            toast("Please enter a name to continue")
+        } else {
+            val name = nameField.text.toString()
+            defaultSharedPreferences.edit().putString(getString(R.string.key_name), name).apply()
             info("Name for client set to " + name)
 
             activity.replaceFragment(R.id.fragment_container, NameFragment())
-        } else {
-            toast("Please enter a name to continue")
         }
     }
 }
